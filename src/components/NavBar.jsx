56cd97952a4b1fe6d77748061186e9eebeb81cf4
bishwa <108havocx108@gmail.com>
2025-04-09 17:34:39 +0545

    implemented Framer-motion for animation & useEffect hook

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { navItems } from "../constant/Constant.jsx";
 import { Menu, X } from "lucide-react";
 import "../index.css";
 import BgLogo from "../assets/bg.svg";
+import { motion, AnimatePresence } from "framer-motion";
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -10,6 +11,9 @@ const NavBar = () => {
   const menuToggler = () => {
     setMenuOpen(!menuOpen);
   };
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "";
+  }, [menuOpen]);
 
   return (
     <nav className="sticky top-0 z-40 backdrop-blur-lg border-b border-white/10">
@@ -30,23 +34,34 @@ const NavBar = () => {
           {/* NavElements for Desktop */}
           <NavElements menuOpen={menuOpen} isMobile={false} />
 
-          {/* Toggle Button for Mobile */}
+          {/* Toggle Button for Mobile with animation */}
           <div className="md:hidden w-6 h-6 mt-6 mr-4 cursor-pointer hover:bg-[#4aa3bc] transition-colors duration-100 rounded-lg">
-            <button
+            <motion.button
               onClick={menuToggler}
+              initial={false}
+              animate={{ rotate: menuOpen ? 180 : 0 }}
+              transition={{ duration: 0.4 }}
               className="text-white md:hidden focus:outline-none"
             >
               {menuOpen ? <X /> : <Menu />}
-            </button>
+            </motion.button>
           </div>
         </div>
 
         {/* NavElements for Mobile (conditionally rendered below) */}
-        {menuOpen && (
-          <div className="md:hidden">
-            <NavElements menuOpen={menuOpen} isMobile={true} />
-          </div>
-        )}
+        <AnimatePresence>
+          {menuOpen && (
+            <motion.div
+              className="md:hidden "
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.2 }}
+            >
+              <NavElements menuOpen={menuOpen} isMobile={true} />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </nav>
   );
@@ -58,13 +73,13 @@ export const NavElements = ({ menuOpen, isMobile }) => {
     <ul
       className={`${
         isMobile
-          ? "flex flex-col items-center space-y-4 p-4 bg-gray-900 w-full absolute left-0 top-16 z-50"
+          ? "flex flex-col  items-center space-y-4 p-4 bg-gray-900 h-screen w-screen absolute  top-20 z-50"
           : "hidden md:flex md:justify-center md:items-center ml-14 space-x-12 mr-[160px]"
       }`}
     >
       {navItems.map((item, index) => (
         <li
-          className="text-white font-[Sour_Gummy] text-[16px] hover:text-[#4aa3bc]"
+          className="text-white font-[Sour_Gummy] md:text-[16px] text-2xl md:mt-0 mt-8 hover:text-[#4aa3bc]"
           key={index}
         >
           <a href={item.href}>{item.label}</a>
