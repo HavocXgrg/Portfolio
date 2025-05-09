812dc69092595e974304c2ddd17b7ae0d0ccca2c
bishwa <108havocx108@gmail.com>
2025-05-09 20:46:50 +0545

    particle component used in herosection for moving line-dot

---
+++ src/components/ParticleBackground.jsx
@@ -0,0 +1,91 @@
+import Particles from "react-tsparticles";
+import { useCallback } from "react";
+import { loadSlim } from "tsparticles-slim"; // Use loadSlim for smaller bundle size
+
+const ParticleBackground = ({ className }) => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadSlim(engine); // Load slim version of tsparticles
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      className={className}
+      init={particlesInit}
+      options={{
+        background: {
+          color: {
+            value: "#000", // Background color
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 2,
+            },
+            repulse: {
+              distance: 150,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff", // Dot color
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.4,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+export default ParticleBackground;
