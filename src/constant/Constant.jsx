395d18aa8255f2c236c858302f73f887c38a9bf5
bishwa <108havocx108@gmail.com>
2025-06-20 13:24:53 +0545

    added icons from react-icons which is constant

--- src/constant/Constant.jsx
+++ src/constant/Constant.jsx
@@ -1,6 +1,28 @@
+import { FaReact, FaHtml5, FaDatabase } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+import { RiTailwindCssFill, RiJavascriptFill } from "react-icons/ri";
+
 export const navItems = [
   { label: "Home", href: "#" },
-  { label: "About Me", href: "#" },
-  { label: "Skills", href: "#" },
-  { label: "Contacts", href: "#" },
+  { label: "About Me", href: "#about" },
+  { label: "Skills", href: "#skills" },
+  { label: "Projects", href: "#projects" },
+  { label: "Contacts", href: "#contact" },
+];
+
+export const skillIcons = [
+  { icon: <FaHtml5 size={40} />, name: "HTML", color: " text-orange-600 " },
+  {
+    icon: <RiJavascriptFill size={40} />,
+    name: "JavaScript",
+    color: "text-[#F7DF1E] ",
+  },
+  { icon: <FaReact size={40} />, name: "React", color: "text-blue-400" },
+  {
+    icon: <RiTailwindCssFill size={40} />,
+    name: "Tailwind",
+    color: "text-blue-400",
+  },
+
+  { icon: <FaDatabase size={40} />, name: "SQL", color: "text-blue-600" },
 ];
