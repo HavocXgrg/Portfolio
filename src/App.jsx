e8224bbb66e3b6044af7062a7d901391fd69af9b
bishwa <108havocx108@gmail.com>
2025-06-25 21:37:40 +0545

    removed skill-component for better nav item

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Herosection from "./components/Herosection";
+import AboutSection from "./components/AboutSection";
+
+import Projects from "./components/Projects";
+
+import Contacts from "./components/Contacts";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+      <Herosection />
+      <AboutSection />
+      <Projects />
+      <Contacts />
+    </div>
+  );
 };
 
 export default App;
